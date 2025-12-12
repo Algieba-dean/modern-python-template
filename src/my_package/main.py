ce89c22608f1269eb4829930d954fb32007a7f6f
algieba <algieba.king@gmail.com>
2025-12-12 21:51:55 +0800

    chore: add documentation generation

--- src/my_package/main.py
+++ src/my_package/main.py
@@ -1,2 +1,13 @@
+"""
+This is demo documentation
+
+"""
+
+
 def main() -> str:
+    """docString demo for documentation generation
+
+    Returns:
+        str: _description_
+    """
     return "Please delete this placeholder file when you initilized a new project."
